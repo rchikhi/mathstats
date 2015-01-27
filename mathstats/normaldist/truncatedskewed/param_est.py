715475fb3e66f91cacb7024aa214ae28551ff151
ksahlin <krsahlin@gmail.com>
2015-01-27 15:46:14 +0100

    continue implementing conditional variance

--- mathstats/normaldist/truncatedskewed/param_est.py
+++ mathstats/normaldist/truncatedskewed/param_est.py
@@ -18,6 +18,7 @@
 
 import sys
 from math import exp, pi, sqrt,log
+import scipy.stats 
 from mathstats.normaldist import normal
 
 
@@ -258,143 +259,203 @@ def tr_sk_std_dev(mean, stdDev, readLen, c1Len, c2Len, d):
     return(std_dev)
 
 
-def a(x,d, mu, sigma, c1Len, c2Len, r):
-    """ integral x min(x-d-2 r+1, a+b+d-x+1, c-r+1) e^(-(x-mu)^2/(2 sigma^2)) dx = piecewise 
+# def a(x,d, mu, sigma, c1Len, c2Len, r):
+#     """ 
+#     integral x max(0, min(x-d-2 r+1, a+b+d-x+1, c-r+1)) e^(-(x-mu)^2/(2 sigma^2)) dx = piecewise | (c-r+1) ((sqrt(pi/2) mu sigma erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e))-(sigma^2 e^(-(x-mu)^2/(2 sigma^2)))/(log(e))) | a+b-c+d+r-x>=0&&c-r>-1&&c+d+r-x<=0
+# -(sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(2 pi) e^((x-mu)^2/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)) (mu log(e) (-a-b-d+mu-1)+sigma^2)+2 sigma sqrt(log(e)) (a+b+d-mu-x+1)))/(2 log^(3/2)(e)) | a+b-c+d+r-x<0&&a+b+2 d+2 r-2 x<=0&&a+b+d-x>-1
+# (sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(2 pi) e^((x-mu)^2/(2 sigma^2)) (mu log(e) (-d+mu-2 r+1)+sigma^2) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma sqrt(log(e)) (-d+mu-2 r+x+1)))/(2 log^(3/2)(e)) | d+2 r-x<1&&a+b+2 d+2 r-2 x>0&&c+d+r-x>0
+# integral x min(x-d-2 r+1, a+b+d-x+1, c-r+1) e^(-(x-mu)^2/(2 sigma^2)) dx = piecewise 
 
-    if a+b-c+d+r-x>=0 && c+d+r-x<=0:
-        (c-r+1) ((sqrt(pi/2) mu sigma erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e))-(sigma^2 e^(-(x-mu)^2/(2 sigma^2)))/(log(e))) 
-        
-    if a+b-c+d+r-x<0&& a+b+2 d+2r-2 x<=0:
-        -(sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(2 pi) e^((x-mu)^2/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)) (mu log(e) (-a-b-d+mu-1)+sigma^2)+2 sigma sqrt(log(e)) (a+b+d-mu-x+1)))/(2 log^(3/2)(e)) 
+#     """
 
-    otherwise:
-        (sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(2 pi) e^((x-mu)^2/(2 sigma^2)) (mu log(e) (-d+mu-2 r+1)+sigma^2) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma sqrt(log(e)) (-d+mu-2 r+x+1)))/(2 log^(3/2)(e)) 
+#     c_max = max(c1Len,c2Len)
+#     c_min = min(c1Len,c2Len)
 
-    """
-    ## specify integration points of interest
-    # place1 = d - 2*readLen + 1
-    # place2 = min(c1Len,c2_len) - readLen + 1 
-    # place3 = max(c1Len,c2_len) + d + readLen
-    # place4 = c1Len + c2Len + d + 1
-    c_max = max(c1Len,c2Len)
-    c_min = min(c1Len,c2Len)
-    if c_max + d + r - x >=0 and c_min + d + r - x <=0:
-        a_term = \
-        (1.0/sqrt(2*pi*sigma**2))* (c_min-r+1) * \
-        (  sqrt(pi/2.0)*mu*sigma*normal.erf((x-mu)/(sqrt(2)*sigma)) \
-        - sigma**2.0 *exp(-(x-mu)**2.0/(2*sigma**2.0)) )
+#     if c_min + d + r <= x <= c_max + d + r  and  r < c_min +1:
+#         print 'a2'
+#         a_term = (c_min-r+1) * \
+#         ( sqrt(pi/2.0)*mu*sigma * erf_norm(x,mu,sigma) - \
+#         sigma**2.0 *e_norm_neg(x,mu,sigma) )
 
-    elif c_max + d + r - x < 0 and c1Len + c2Len + 2*d + 2*r - 2*x <=0:
-        a_term = \
-        - (1.0/sqrt(2*pi*sigma**2))*(1/2.0) * sigma* exp(-(x-mu)**2.0/(2.0*sigma**2.0)) * \
-        ( sqrt(2*pi) *exp( (x-mu)**2 / (2*sigma**2) ) * normal.erf( (x-mu)/(sqrt(2)*sigma)) * \
-        (mu * (-c1Len - c2Len - d + mu - 1) + sigma**2.0) \
-        + 2*sigma* (c1Len + c2Len + d - mu - x + 1) )
+#     elif c_max + d + r < x < c_max +c_min + d + 1 and ( (c_max + c_min)/2.0 + d + r <= x):
+#         print 'a3'
+#         a_term = - (1/2.0) * sigma * e_norm_neg(x,mu,sigma) * \
+#         ( sqrt(2*pi)* e_norm_neg(x,mu,sigma) * erf_norm(x,mu,sigma) * (mu * (-c1Len - c2Len - d + mu - 1) + sigma**2) +\
+#         2*sigma* (c1Len + c2Len + d - mu - x + 1) )
 
-    else:
-        a_term = (1.0/sqrt(2*pi*sigma**2))*(1/2.0) * (sigma * exp(-(x-mu)**2/(2*sigma**2))) *  (sqrt(2*pi)*exp((x-mu)**2/(2*sigma**2))*(mu*(-d+mu-2*r+1)+sigma**2)*normal.erf((x-mu)/(sqrt(2)*sigma))- 2*sigma*(-d+mu-2*r+x+1)) 
-    return a_term
-
-
-
-def b(x, d, mu, sigma, c1Len, c2Len, r):
-    """
-    integral (x^2 min(x-d-2 r+1, a+b+d-x+1, c-r+1))/e^((x-mu)^2/(2 sigma^2)) dx = piecewise
-    
-    if  a+b-c+d+r-x>=0 && c+d+r-x<=0 :
-        (sigma (c-r+1) e^(-(mu^2+x^2)/(2 sigma^2)) (sqrt(2 pi) (mu^2 log(e)+sigma^2) e^((mu^2+x^2)/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma sqrt(log(e)) (mu+x) e^((mu x)/sigma^2)))/(2 log^(3/2)(e)) 
-    
-    elif a+b-c+d+r-x<0 && a+b+2 d+2r-2 x<=0:
-        (sigma e^(-(mu^2+x^2)/(2 sigma^2)) (2 sigma e^((mu x)/sigma^2) (log(e) (mu (-a-b-d+mu-1)-x (a+b+d-mu+1)+x^2)+2 sigma^2)-sqrt(2 pi) sqrt(log(e)) e^((mu^2+x^2)/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)) (mu^2 log(e) (-a-b-d+mu-1)-sigma^2 (a+b+d-3 mu+1))))/(2 log^2(e)) 
-    
-    else:
-        (sigma e^(-(mu^2+x^2)/(2 sigma^2)) (sqrt(2 pi) sqrt(log(e)) e^((mu^2+x^2)/(2 sigma^2)) (mu^2 log(e) (-d+mu-2 r+1)-sigma^2 (d-3 mu+2 r-1)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma e^((mu x)/sigma^2) (log(e) (mu (-d+mu-2 r+1)+x (-d+mu-2 r+1)+x^2)+2 sigma^2)))/(2 log^2(e)) 
-    """
-    c_max = max(c1Len,c2Len)
-    c_min = min(c1Len,c2Len)
-    if c_max + d + r - x >=0 and c_min + d + r - x <=0:
-        b_term = \
-        (1.0/sqrt(2*pi*sigma**2)) *(1/2.0)*sigma*(c_min-r+1)* exp(-(mu**2+x**2)/(2*sigma**2)) *\
-        (sqrt(2*pi)*(mu**2 + sigma**2) * exp((mu**2+x**2)/(2*sigma**2))*normal.erf( (x-mu)/(sqrt(2)*sigma)) \
-        - 2*sigma*(mu+x)*exp((mu*x)/sigma**2) )
-   
-    elif c_max + d + r - x < 0 and c1Len + c2Len + 2*d + 2*r - 2*x <=0:
-        b_term = \
-        (1.0/sqrt(2*pi*sigma**2))*(1/2.0)*sigma *  exp(-(mu**2+x**2)/(2*sigma**2)) *\
-        (2*sigma*exp((mu*x)/sigma**2)*((mu *(-c1Len- c2Len -d+mu-1)- x*(c1Len+ c2Len +d-mu+1) + \
-        x**2)+2*sigma**2) - sqrt(2*pi) *exp((mu**2+x**2)/(2*sigma**2))* \
-        normal.erf((x-mu)/(sqrt(2)*sigma)) *(mu**2 *(-c1Len- c2Len -d+mu-1) - sigma**2 * (c1Len+ c2Len +d-3*mu+1))) 
-    
-    else:
-        b_term = \
-        (1.0/sqrt(2*pi*sigma**2))*(1/2.0)*sigma *  exp(-(mu**2+x**2)/(2*sigma**2)) *\
-        (sqrt(2*pi)*exp((mu**2+x**2)/(2*sigma**2)) *  (mu**2 * (-d+mu-2*r+1) - sigma**2 *(d-3*mu+ \
-        2*r-1))*normal.erf((x-mu)/(sqrt(2.0)*sigma))-2*sigma*exp((mu*x)/sigma**2)* \
-        ( ( mu*(-d+mu-2*r+1)+ x*(-d+mu-2*r+1)+x**2)+2*sigma**2))
-    return b_term
-
-
-def g_d(x, d, mu, sigma, c1Len, c2Len, r):
-    """
-        integral min(x-d-2 r+1, a+b+d-x+1, c-r+1) e^(-(x-mu)^2/(2 sigma^2)) dx = piecewise 
-        if  a+b-c+d+r-x>=0&&c+d+r-x<=0:
-            (sqrt(pi/2) sigma (c-r+1) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e)) 
-        elif  a+b-c+d+r-x<0&&a+b+2 d+2 r-2 x<=0:
-
-            (sqrt(pi/2) sigma (a+b+d-mu+1) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e))+(sigma^2 e^(-(x-mu)^2/(2 sigma^2)))/(log(e))
-        else:
-            -(sqrt(pi/2) sigma (d-mu+2 r-1) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e))-(sigma^2 e^(-(x-mu)^2/(2 sigma^2)))/(log(e))
-    """
-    c_max = max(c1Len,c2Len)
-    c_min = min(c1Len,c2Len)
-
-    if c_max + d + r - x >=0 and c_min + d + r - x <=0:
-        print 'g_d1'
-        g_d_term = \
-        sqrt(pi/2.0) * (1.0/sqrt(2*pi*sigma**2)) * sigma *(c_min-r+1)*normal.erf( (x-mu)/(sqrt(2.0)*sigma))
-    
-    elif c_max + d + r - x < 0 and c1Len + c2Len + 2*d + 2*r - 2*x <=0:
-        print 'g_d2'
-        g_d_term = \
-        (1.0/sqrt(2*pi*sigma**2)) * \
-        ( sqrt(pi/2.0) * sigma *(c1Len+c2Len+d-mu+1)* normal.erf((x-mu)/(sqrt(2.0)*sigma)) +\
-        sigma**2* exp(-(x-mu)**2/(2*sigma**2)))
-    else:
-        print 'g_d3'
-        g_d_term = \
-        -(1.0/sqrt(2*pi*sigma**2)) * \
-        ( sqrt(pi/2.0) * sigma *(d-mu+2*r-1)* normal.erf((x-mu)/(sqrt(2.0)*sigma)) -\
-        sigma**2* exp(-(x-mu)**2/(2*sigma**2)))
+#     elif d+2*r-1 <= x < c_min + d + r  and x < (c_max + c_min)/2.0 + d + r :
+#         print 'a1'
+#         a_term = (1/2.0) * sigma * e_norm_neg(x,mu,sigma) * \
+#         ( sqrt(2*pi)* e_norm_pos(x,mu,sigma) * erf_norm(x,mu,sigma) *(mu * (-d+mu-2*r+1) + sigma**2) -\
+#         2*sigma* (-d+mu-2*r+x+1)    )
+#     else:
+#         a_term = 0
 
     
-    return g_d_term
+#     # scale result with the constant in the normal distribution
+#     a_term = (1.0/sqrt(2*pi*sigma**2)) *a_term
+#     return a_term
+
+
+
+# def b(x, d, mu, sigma, c1Len, c2Len, r):
+#     """
+# integral (x^2 max(0, min(x-d-2 r+1, a+b+d-x+1, c-r+1)))/e^((x-mu)^2/(2 sigma^2)) dx = piecewise | (sigma (c-r+1) e^(-(mu^2+x^2)/(2 sigma^2)) (sqrt(2 pi) (mu^2 log(e)+sigma^2) e^((mu^2+x^2)/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma sqrt(log(e)) (mu+x) e^((mu x)/sigma^2)))/(2 log^(3/2)(e)) | a+b-c+d+r-x>=0&&c-r>-1&&c+d+r-x<=0
+# (sigma e^(-(mu^2+x^2)/(2 sigma^2)) (2 sigma e^((mu x)/sigma^2) (log(e) (mu (-a-b-d+mu-1)-x (a+b+d-mu+1)+x^2)+2 sigma^2)-sqrt(2 pi) sqrt(log(e)) e^((mu^2+x^2)/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)) (mu^2 log(e) (-a-b-d+mu-1)-sigma^2 (a+b+d-3 mu+1))))/(2 log^2(e)) | a+b-c+d+r-x<0&&a+b+2 d+2 r-2 x<=0&&a+b+d-x>-1
+# (sigma e^(-(mu^2+x^2)/(2 sigma^2)) (sqrt(2 pi) sqrt(log(e)) e^((mu^2+x^2)/(2 sigma^2)) (mu^2 log(e) (-d+mu-2 r+1)-sigma^2 (d-3 mu+2 r-1)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))-2 sigma e^((mu x)/sigma^2) (log(e) (mu (-d+mu-2 r+1)+x (-d+mu-2 r+1)+x^2)+2 sigma^2)))/(2 log^2(e)) | d+2 r-x<1&&a+b+2 d+2 r-2 x>0&&c+d+r-x>0+constant
+
+#     """
+
+#     c_max = max(c1Len,c2Len)
+#     c_min = min(c1Len,c2Len)
+#     if c_min + d + r <= x < c_max + d + r  and  r < c_min +1:
+#         print 'b2'
+#         b_term = sigma*(c_min-r+1)* e_sq_norm_neg(x,mu,sigma) * \
+#         ( sqrt(2*pi)*(mu**2 + sigma**2)* e_sq_norm_pos(x,mu,sigma)* erf_norm(x,mu,sigma) -\
+#          2*sigma *(mu + x)* e_times_pos(x,mu,sigma))   
+         
+       
+#     elif c_max + d + r < x <= c_max +c_min + d + 1 and ( (c_max + c_min)/2.0 + d + r <= x):
+#         print 'b3'
+#         b_term = (1/2.0)*sigma* e_sq_norm_neg(x,mu,sigma) * \
+#         ( 2*sigma*e_times_pos(x,mu,sigma) * \
+#         ( (mu *(-c1Len- c2Len -d+mu-1)- x*(c1Len+ c2Len +d-mu+1) +x**2)+2*sigma**2) -\
+#         sqrt(2*pi)*e_sq_norm_pos(x,mu,sigma)*erf_norm(x,mu,sigma)* \
+#          (mu**2 *(-c1Len- c2Len -d+mu-1) - sigma**2 * (c1Len+ c2Len +d-3*mu+1))  )
+
+    
+#     elif d+2*r-1 <= x < c_min + d + r  and x < (c_max + c_min)/2.0 + d + r :
+#         print 'b1'
+#         b_term = (1/2.0)*sigma* e_sq_norm_neg(x,mu,sigma) * \
+#         ( sqrt(2*pi)*e_sq_norm_pos(x,mu,sigma) * \
+#         ( mu**2 * (-d+mu-2*r+1) - sigma**2 *(d-3*mu+ 2*r-1)) * erf_norm(x,mu,sigma) -  \
+#         2*sigma*e_times_pos(x,mu,sigma)* ( ( mu*(-d+mu-2*r+1)+ x*(-d+mu-2*r+1) + x**2)+ 2*sigma**2) )
+#         # *(1/2.0)*sigma *  exp(-(mu**2+x**2)/(2*sigma**2)) *\
+#         # (sqrt(2*pi)*exp((mu**2+x**2)/(2*sigma**2)) *  (mu**2 * (-d+mu-2*r+1) - sigma**2 *(d-3*mu+ \
+#         # 2*r-1))*normal.erf((x-mu)/(sqrt(2.0)*sigma))-2*sigma*exp((mu*x)/sigma**2)* \
+#         # ( ( mu*(-d+mu-2*r+1)+ x*(-d+mu-2*r+1)+x**2)+2*sigma**2))
+
+#     else:
+#         b_term = 0
+
+#     b_term = (1.0/sqrt(2*pi*sigma**2)) *b_term
+
+#     return b_term
+
+def erf_norm(x,mu,sigma):
+    return normal.erf((x-mu)/(sqrt(2.0)*sigma))    
+
+def e_norm_neg(x,mu,sigma):
+    return scipy.stats.norm.pdf(x,mu,sigma) # exp(-(x-mu)**2/(2*sigma**2))
+
+def e_norm_pos(x,mu,sigma):
+    return exp((x-mu)**2/(2*sigma**2))
+
+def e_sq_norm_pos(x,mu,sigma):
+    return exp((mu**2+x**2)/(2*sigma**2))
+
+def e_sq_norm_neg(x,mu,sigma):
+    return exp(-(mu**2+x**2)/(2*sigma**2))
+
+def e_times_pos(x,mu,sigma):
+    return exp((mu*x)/sigma**2)
+
+
+# def g_d(x, d, mu, sigma, c1Len, c2Len, r):
+#     """
+# g(d):
+# piecewise | (sqrt(pi/2) sigma (c-r+1) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma)))/sqrt(log(e)) | r<c+1&&x>=c+d+r&&x<=a+b-c+d+r
+# (sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(log(e)) (sqrt(2 pi) a+sqrt(2 pi) b+sqrt(2 pi) d-sqrt(2 pi) mu+sqrt(2 pi)) e^((x-mu)^2/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))+2 sigma))/(2 log(e)) | x<a+b+d+1&&x>=1/2 (a+b+2 d+2 r)&&x>a+b-c+d+r
+# -(sigma e^(-(x-mu)^2/(2 sigma^2)) (sqrt(log(e)) (sqrt(2 pi) d-sqrt(2 pi) mu+2 sqrt(2 pi) r-sqrt(2 pi)) e^((x-mu)^2/(2 sigma^2)) erf((sqrt(log(e)) (x-mu))/(sqrt(2) sigma))+2 sigma))/(2 log(e)) | x<1/2 (a+b+2 d+2 r)&&x<c+d+r&&x>d+2 r-1+constant
+#     """
+#     c_max = max(c1Len,c2Len)
+#     c_min = min(c1Len,c2Len)
+
+#     if  c_min + d + r <= x < c_max + d + r  and  r < c_min +1:
+#         print 'g_d2'
+#         g_d_term = sqrt(pi/2.0) *  sigma *(c_min-r+1)*erf_norm(x,mu,sigma)
+    
+#     elif  c_max + d + r < x <= c_max +c_min + d + 1 and ( (c_max + c_min)/2.0 + d + r <= x) : 
+#         print 'g_d3'
+#         g_d_term = 1/2.0 *sigma*e_norm_neg(x,mu,sigma) *\
+#         ( sqrt(pi*2.0) * (c1Len+c2Len+d-mu+1)* \
+#         e_norm_neg(x,mu,sigma)* erf_norm(x,mu,sigma) + 2*sigma)
+#     elif d+2*r-1 <= x < c_min + d + r  and x < (c_max + c_min)/2.0 + d + r :
+#         print 'g_d1'
+#         g_d_term = - sqrt(pi/2.0) *sigma* (d-mu+2*r-1) *erf_norm(x,mu,sigma) - \
+#         sigma**2 * e_norm_neg(x,mu,sigma) 
+#     else:
+#         g_d_term = 0
+
+#     # scale result with the constant in the normal distribution
+#     g_d_term = (1.0/sqrt(2*pi*sigma**2)) * g_d_term
+#     return g_d_term
+
+
+ 
+def b1(x,mu,sigma,r,d,c_min,c_max):
+    return
+def b2(x,mu,sigma,r,d,c_min,c_max):
+    return
+def b3(x,mu,sigma,r,d,c_min,c_max):
+    return
+
+
 
 def stddev_given_d(mean, stdDev, readLen, c1Len, c2Len, d):
     """
         b/g_d - (a/g_d)^2
     """
-    if c1Len + c2Len + d + 1 > mean + 5*stdDev:
-        upper_integration_point = mean + 5*stdDev
-    else:
-        upper_integration_point = c1Len + c2Len + d + 1 
+    stdDev = float(stdDev)
+    mean = float(mean)
+    c_min = min(c1Len,c2Len)
+    c_max = max(c1Len,c2Len)
 
-    lower_integration_point =  d + 2 * readLen - 1
+    assert c_min > readLen - 1
+
+    # define integration points (x1, x2, x3, x4) 
+    # the functions a, b and g_d each have three different 
+    # intervals they need to be integrated in (because of the max(min(.,.,.)) function)
+    # intervals: [x1,x2], [x2,x3], [x3,x4]. 
+    x1 = d + 2 * readLen - 1
+    x2 = c_min + d +readLen if c_min + d + readLen < mean + 5*stdDev else mean + 5*stdDev
+    x3 = c_max + d +readLen if c_max + d + readLen < mean + 5*stdDev else mean + 5*stdDev
+    x4 =  c_max +c_min + d + 1 if c_min + c_max + d + 1 < mean + 5*stdDev else mean + 5*stdDev
+
+    b_term = b1(x2) - b1(x_1) + b2(x3) - b2(x2) + b3(x4) - b3(x3)
+   
+    # # if c1Len + c2Len + d + 1 > mean + 5*stdDev:
+    # #     upper_integration_point = mean + 5*stdDev
+    # # else:
+    # #     upper_integration_point = c1Len + c2Len + d + 1 
+
+    # # lower_integration_point =  d + 2 * readLen - 1
     
-    b_ = b(upper_integration_point, d, mean, stdDev, c1Len, c2Len, readLen) - b(lower_integration_point, d, mean, stdDev, c1Len, c2Len, readLen)
-    a_ = a(upper_integration_point, d, mean, stdDev, c1Len, c2Len, readLen) - a(lower_integration_point, d, mean, stdDev, c1Len, c2Len, readLen)
-    g_d_lower = g_d(lower_integration_point,d, mean, stdDev, c1Len, c2Len, readLen)
-    g_d_upper = g_d(upper_integration_point,d, mean, stdDev, c1Len, c2Len, readLen)
-    g_d_area = g_d_upper - g_d_lower
-    print g_d_lower,g_d_upper
-    print a_, g_d_area, a_/ g_d_area,  ( a_ / g_d_area )**2  
-    print b_, g_d_area, b_/ g_d_area, b_ / g_d_area
+    # b_upper = b(upper_integration_point, d, mean, stdDev, c1Len, c2Len, readLen) 
+    # b_lower = b(lower_integration_point, d, mean, stdDev, c1Len, c2Len, readLen)
+    # a_upper = a(upper_integration_point, d, mean, stdDev, c1Len, c2Len, readLen)
+    # a_lower =  a(lower_integration_point, d, mean, stdDev, c1Len, c2Len, readLen)
+    # g_d_upper = g_d(upper_integration_point,d, mean, stdDev, c1Len, c2Len, readLen)
+    # g_d_lower = g_d(lower_integration_point,d, mean, stdDev, c1Len, c2Len, readLen)
 
-    var = b_ / g_d_area\
-    - ( a_ / g_d_area )**2
+    # g_d_area = g_d_upper - g_d_lower
+    # b_area = b_upper - b_lower
+    # a_area = a_upper - a_lower
+
+    # print g_d_lower,g_d_upper
+    # print a_lower,a_upper
+    # print b_lower,b_upper
+    
+    # print a_area,  ( a_area / g_d_area )**2  
+    # print b_area,  b_area/ g_d_area
+    # print g_d_area
+
+    var = b_area / g_d_area\
+    - ( a_area / g_d_area )**2
 
     print var
     if var < 0:
-        sys.stderr.write('Error in estimate, a={0}, b={1}'.format(a_,b_))
+        sys.stderr.write('Error in estimate, a={0}, b={1}'.format(a_area,b_area))
         std_dev_est = 0
     else:
         std_dev_est = sqrt(var)  
