4714333d58a9f388bed118059faa94092f83c39f
ksahlin <krsahlin@gmail.com>
2015-10-08 18:12:34 +0100

    ctd implemnenting on lognormal gapest fit

--- mathstats/log_normal_param_est.py
+++ mathstats/log_normal_param_est.py
@@ -29,87 +29,73 @@ def math_log_if_pos(x):
 	else:
 		print 'here'
 		inf = float("inf")
-		return inf
+		return -inf
 
-def calc_g_of_d_ratio(d, mu, sigma, c1Len, c2Len, readLen):
-    c_min = min(c1Len, c2Len)
-    c_max = max(c1Len, c2Len)
+def calc_g_prim_d(d, mu, sigma, c_min, c_max, c1Len, c2Len, r, cutoff_approx_level):
+	if d < 0:
+		d= -d*2
+	x1 = d+2*r if d+2*r < cutoff_approx_level else cutoff_approx_level
+	x2 = c_min + d + r - 1 if c_min + d + r - 1 < cutoff_approx_level else cutoff_approx_level
+	x3 = c_min + d + r if c_min + d + r < cutoff_approx_level else cutoff_approx_level
+	x4 = c_max + d + r if c_max + d + r < cutoff_approx_level else cutoff_approx_level
+	x5 = c_max + d + r + 1 if c_max + d + r + 1 < cutoff_approx_level else cutoff_approx_level
+	x6 = c_min + c_max + d if c_min + c_max + d < cutoff_approx_level else cutoff_approx_level
 
-    def Nominator(d, c_min, c_max, c1Len, c2Len, readLen):
-        term1 = -0.5 * (normal.erf(( math_log_if_pos(c_min + c_max + d + 1) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf((math_log_if_pos(d + 2 * readLen - 1) - mu) / (2 ** 0.5 * float(sigma))))
-        term2 = +0.5 * (normal.erf(( math_log_if_pos(d + c_max + readLen) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf(( math_log_if_pos(d + c_min + readLen) - mu) / (2 ** 0.5 * float(sigma))))
-        g_prime_d = term1 + term2
-        return -g_prime_d
-
-    def Denominator(d, c1Len, c2Len, readLen):
-    	#print 'lol', d, c1Len, c2Len, readLen
-        #term 1,2 and 3 denodes what part of the function we are integrating term1 for first (ascending), etc...
-
-        term1 = (c_min - readLen + 1) / 2.0 * (normal.erf(( math_log_if_pos(c_max + d + readLen) - mu) / ((2 ** 0.5) * sigma)) - normal.erf(( math_log_if_pos(c_min + d + readLen) - mu) / ((2 ** 0.5) * sigma)))
-
-        term2 = (c_min + c_max + d - mu + 1) / 2.0 * (normal.erf(( math_log_if_pos(c_min + c_max + d + 1) - mu) / (2 ** 0.5 * float(sigma))) - normal.erf(( math_log_if_pos(c_max + readLen + d) - mu) / (2 ** 0.5 * float(sigma))))
-
-        term3 = (d + 2 * readLen - mu - 1) / 2.0 * (normal.erf(( math_log_if_pos(d + 2 * readLen - 1) - mu) / (2 ** 0.5 * float(sigma))) - normal.erf(( math_log_if_pos(c_min + d + readLen) - mu) / ((2 ** 0.5) * sigma)))
-
-        term4 = sigma / ((2 * pi) ** 0.5) * (exp(-(( math_log_if_pos(c_min + c_max + d + 1) - mu) ** 2) / (float(2 * sigma ** 2))) + exp(-((math_log_if_pos(d + 2 * readLen - 1) - mu) ** 2) / (float(2 * sigma ** 2))))
-
-        term5 = -sigma / ((2 * pi) ** 0.5) * (exp(-((math_log_if_pos(c_max + readLen + d) - mu) ** 2) / (float(2 * sigma ** 2))) + exp(-(( math_log_if_pos(c_min + readLen + d) - mu) ** 2) / (float(2 * sigma ** 2))))
-        g_d = term1 + term2 + term3 + term4 + term5
-        print term1, term2,  term3,  term4,  term5
-        return g_d
-
-    denominator = Denominator(d, c1Len, c2Len, readLen)
-    nominator = Nominator(d, c_min, c_max, c1Len, c2Len, readLen)
-    print denominator, nominator
-
-    try:
-        g_of_d_ratio = nominator / denominator
-    except ZeroDivisionError:
-        g_of_d_ratio = 0.0000000001
-
-    return g_of_d_ratio
-
-
-def calc_g_prim_d(d, mu, sigma, c_min, c_max, c1Len, c2Len, r):
-    term1 = -0.5 * (normal.erf(( math_log_if_pos(c_min + c_max + d + 1) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf((math_log_if_pos(d + 2 * r - 1) - mu) / (2 ** 0.5 * float(sigma))))
-    term2 = +0.5 * (normal.erf(( math_log_if_pos(d + c_max + r) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf(( math_log_if_pos(d + c_min + r) - mu) / (2 ** 0.5 * float(sigma))))
-    g_prime_d = term1 + term2
-    return g_prime_d
+	term1 = -0.5 * (normal.erf(( math_log_if_pos(x6 + 1) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf((math_log_if_pos(x1 - 1) - mu) / (2 ** 0.5 * float(sigma))))
+	term2 = +0.5 * (normal.erf(( math_log_if_pos(x4) - mu) / (2 ** 0.5 * float(sigma))) + normal.erf(( math_log_if_pos(x3) - mu) / (2 ** 0.5 * float(sigma))))
+	g_prime_d = term1 + term2
+	return -g_prime_d
 
 def gd(x, d, mu, sigma, c_min, c_max, r):
-	#x = o + d
-	
 	# in the middle part (roof of triangle house, between c_min and c_max)
 	if c_max+d+r-x >= 0 and c_min+d+r-x <= 0:
-		return ((c_min-r+1)/2.0 * normal.erf((math_log_if_pos(x)-mu))/(math.sqrt(2.0)*sigma))
+		return (c_min-r+1)/2.0 * normal.erf((math_log_if_pos(x)-mu)/(math.sqrt(2.0)*sigma))
 
 	# x is large, on the down slope of the triangle house
 	elif c_max+d+r-x < 0 and c_min + c_max + d - x > -1:
-		return 1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf((mu-math_log_if_pos(x))+sigma**2)/(sqrt(2) *sigma) - (c_min+c_max+d+1)*normal.erf((mu-math_log_if_pos(x))/(math.sqrt(2.0)*sigma)))
+		#print 'loolz', normal.erf((mu-math_log_if_pos(x))+sigma**2/(sqrt(2) * sigma)), x #normal.erf((mu-math_log_if_pos(x))/(math.sqrt(2.0)*sigma))
+		return 1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf((mu-math_log_if_pos(x))+sigma**2/(sqrt(2) * sigma)) - (c_min+c_max+d+1)*normal.erf((mu-math_log_if_pos(x))/(math.sqrt(2.0)*sigma)))
 
 	# x is small, beginning of triangle house
 	elif  d+2*r-x < 1 and c_min+d+r-x > 0:
+		#print 'laal', normal.erf(((mu-math_log_if_pos(x))+sigma**2)/(math.sqrt(2.0)*sigma))
 		return -1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf(((mu-math_log_if_pos(x))+sigma**2)/(math.sqrt(2.0)*sigma)) - (d+2*r-1)*normal.erf((mu-math_log_if_pos(x)) / (math.sqrt(2.0)*sigma)))
 
 	else:
 		return 0
 
 
-def calc_gd(d, mu, sigma, c_min, c_max, r):
+def calc_gd(x x might need to go in here!! x=d+o, d, mu, sigma, c_min, c_max, r, cutoff_approx_level):
+
+	"""
+		4 different itegration points of the trapetzoid house
+		from 1 possible placement.
+		Values can get shaky too far out on the log normal distribution due to float aritmethic
+		cutoff_approx_level is the upper point where estimations are still stable
+	"""
+	if d < 0:
+		d= -d*2
+	x1 = d+2*r if d+2*r < cutoff_approx_level else cutoff_approx_level
+	x2 = c_min + d + r - 1 if c_min + d + r - 1 < cutoff_approx_level else cutoff_approx_level
+	x3 = c_min + d + r if c_min + d + r < cutoff_approx_level else cutoff_approx_level
+	x4 = c_max + d + r if c_max + d + r < cutoff_approx_level else cutoff_approx_level
+	x5 = c_max + d + r + 1 if c_max + d + r + 1 < cutoff_approx_level else cutoff_approx_level
+	x6 = c_min + c_max + d if c_min + c_max + d < cutoff_approx_level else cutoff_approx_level
+
 	#part_1 =  from x = d+2*r to x = c_min + d + r
-	part_1 = gd(c_min + d + r - 1, d, mu, sigma, c_min, c_max, r) - gd(d+2*r, d, mu, sigma, c_min, c_max, r)
-	print c_min + d + r - 1, d + 2*r
+	part_1 = gd(x2, d, mu, sigma, c_min, c_max, r) - gd(x1, d, mu, sigma, c_min, c_max, r)
 	print gd(c_min + d + r - 1, d, mu, sigma, c_min, c_max, r), gd(d + 2*r, d, mu, sigma, c_min, c_max, r)
 
 	#part_2 = from x = c_min + d + r to x = c_max + d + r
-	part_2 =  gd(c_max + d + r, d, mu, sigma, c_min, c_max, r) - gd(c_min + d + r, d, mu, sigma, c_min, c_max, r)
+	part_2 =  gd(x4, d, mu, sigma, c_min, c_max, r) - gd(x3, d, mu, sigma, c_min, c_max, r)
 
 	print gd(c_max + d + r, d, mu, sigma, c_min, c_max, r), gd(c_min + d + r, d, mu, sigma, c_min, c_max, r)
 
 	#part_3 = from x = c_max + d + r + 1 to c_min + c_max + d 
-	part_3 = gd(c_min + c_max + d, d, mu, sigma, c_min, c_max, r) - gd(c_max + d + r + 1, d, mu, sigma, c_min, c_max, r)
-	print gd(c_min + c_max + d, d, mu, sigma, c_min, c_max, r), gd(c_max + d + r + 1, d, mu, sigma, c_min, c_max, r)
+	part_3 = gd(x6, d, mu, sigma, c_min, c_max, r) - gd(x5, d, mu, sigma, c_min, c_max, r)
 
+	print gd(c_min + c_max + d, d, mu, sigma, c_min, c_max, r), gd(c_max + d + r + 1, d, mu, sigma, c_min, c_max, r)
+	print 'from to', c_min + c_max + d, c_max + d + r + 1
 	print part_1
 	print part_2
 	print part_3
@@ -132,36 +118,99 @@ def GapEstimator(mean, sigma, read_length, observations, c1_len, c2_len=None, em
 
 
 def CalcMLvaluesOfdGeneral(mu, sigma, r, c1Len, observations, c2Len, emperical_max):
-    """
-        returns the ML-gap as float. The ML gap is searched
-        for with an accuracy of 0.1bp. This is NOT the accuracy of the ML-estimate
-        however since it depends on the number of samples etc.
-    """
+	"""
+	    returns the ML-gap as float. The ML gap is searched
+	    for with an accuracy of 0.1bp. This is NOT the accuracy of the ML-estimate
+	    however since it depends on the number of samples etc.
+	"""
+	# Some pruning of extreme observations would be good to have here
+	min_obs = min(observations)
+	max_obs = max(observations)
 
+	n = len(observations)
+	d_ML = 'lol'
+	c_min = min(c1Len, c2Len)
+	c_max = max(c1Len, c2Len)
+	min_gap = -int(min_obs) + int(2*r)
 
-    min_obs = min(observations)
-    max_obs = max(observations)
-    n = len(observations)
-    d_ML = 'lol'
-    c_min = min(c1Len, c2Len)
-    c_max = max(c1Len, c2Len)
-    min_gap = -int(min_obs) + int(2*r)
-    for d_ in range(min_gap, emperical_max, 100):
+	# set max to mean + 2*sd
+	cutoff_approx_level = math.exp(mu + 0.5*sigma**2) + 5*math.sqrt( (math.exp(sigma**2) - 1) * math.exp(2*mu + sigma**2) ) 
+	upper_quantile = math.exp(mu + 0.5*sigma**2) + 2*math.sqrt( (math.exp(sigma**2) - 1) * math.exp(2*mu + sigma**2) )
+	max_gap = upper_quantile if upper_quantile + max_obs <  cutoff_approx_level else cutoff_approx_level - max_obs  #emperical_max
+	d_lower = min_gap
+	d_upper = max_gap
+	print d_upper, cutoff_approx_level
+
+	# while d_upper - d_lower > 0.1:
+	# 	d_ML = (d_upper + d_lower) / 2.0
+	# 	d_ML_2 = d_ML + 5
+	# 	print 'D_MAX:', d_upper, "D_MIN:", d_lower
+	# 	print 'CURRENT GAP:', d_ML
+	# 	other_term = calc_log_norm(observations, d_ML, mu, sigma)
+	# 	g_d = calc_gd(d_ML, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+	# 	g_prime_d = calc_g_prim_d(d_ML, mu, sigma, c_min, c_max, c1Len, c2Len, r, cutoff_approx_level)
+	# 	g_d_ratio = n*g_prime_d/g_d
+	# 	print 'gap:',d_ML, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
+
+	# 	if g_d_ratio < other_term:
+	# 		d_upper = d_ML
+	# 	else:
+	# 		d_lower = d_ML
+
+	# d_ML = (d_upper + d_lower) / 2.0
+
+	x = []
+	y = []
+	y_1 = []
+	y_2 = []
+	g_d_list = []
+	g_prim_d_list = []
+
+	for d_ in range(d_lower, int(d_upper), 100):
 		#for frac in range(10):
 		d = d_ # + frac/10.0
-		g_d = calc_gd(d, mu, sigma, c_min, c_max, r)
-		g_prime_d = calc_g_prim_d(d, mu, sigma, c_min, c_max, c1Len, c2Len, r)
+		#print "GAP", d
+		g_d = calc_gd(d, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_d_list.append(g_d)
+		g_prime_d = calc_g_prim_d(d, mu, sigma, c_min, c_max, c1Len, c2Len, r, cutoff_approx_level)
+		g_prim_d_list.append(g_prime_d)
 
 		g_d_ratio = n*g_prime_d/g_d
 		other_term = calc_log_norm(observations, d, mu, sigma)
-		if 0.95 < g_d_ratio /calc_log_norm(observations, d, mu, sigma) < 1.05:
-			d_ML = d
-			print 'MLGAP', d
+		x.append(d_)
+		y.append(g_d_ratio - other_term)
+		y_1.append(g_d_ratio)
+		y_2.append(other_term)
+
+		# if 0.95 < g_d_ratio /calc_log_norm(observations, d, mu, sigma) < 1.05:
+		# 	d_ML = d
+		# 	print 'MLGAP', d
 
 
 		print 'gap:',d, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
+	import matplotlib.pyplot as plt
+	plt.plot(x,y,'-')
+	x1,x2,y1,y2 = plt.axis()
+	plt.axis((x1,x2,-0.01,0.001))
+	plt.show()
 
-    print 'MLGAP', d_ML 
+	plt.plot(x,y_1,'-')
+	plt.plot(x,y_2,'-r')
+	x1,x2,y1,y2 = plt.axis()
+	plt.axis((x1,x2,-0.01,0.001))
+	plt.show()
+
+	plt.plot(x,g_prim_d_list,'-')
+	x1,x2,y1,y2 = plt.axis()
+	plt.axis((x1,x2,-0.01,0.001))
+	plt.show()
+
+	plt.plot(x,g_d_list,'-r')
+	x1,x2,y1,y2 = plt.axis()
+	plt.axis((x1,x2,-0.01,0.001))
+	plt.show()
+
+	print 'MLGAP', d_ML
 
 
     # min_obs = min(observations)
