99dc9d6773e37714fd82e8e109fa1d05fd8201a1
ksahlin <krsahlin@gmail.com>
2015-10-15 12:58:22 -0400

    pushing a buggy N-R implementation...

--- mathstats/log_normal_param_est.py
+++ mathstats/log_normal_param_est.py
@@ -20,7 +20,6 @@ from mathstats.normaldist import normal
 
 def calc_log_norm(observations, d, mu, sigma):
 	val = sum([ (mu - sigma**2 - math.log(o+d)) / (sigma**2 * (o + d)) for o in observations])
-	print 'VAL:', val
 	return val
 
 
@@ -32,7 +31,7 @@ def math_log_if_pos(x):
 		inf = float("inf")
 		return -inf
 
-# def calc_g_prim_d_OLD(d, mu, sigma, c_min, c_max, c1Len, c2Len, r, cutoff_approx_level):
+# def calc_g_prim_d_OLD(d, mu, sigma, c_min, c_max, c_min, c2_len, r, cutoff_approx_level):
 
 # 	x1 = d+2*r if d+2*r > 0 else 0
 # 	x1 = d+2*r if d+2*r < cutoff_approx_level else cutoff_approx_level
@@ -129,7 +128,7 @@ def calc_gd(d, mu, sigma, c_min, c_max, r, cutoff_approx_level):
 		Values can get shaky too far out on the log normal distribution due to float aritmethic
 		cutoff_approx_level is the upper point where estimations are still stable
 	"""
-	print 'CMON', d+2*r, c_min + d + r, c_max + d + r, c_min + c_max + d
+	#print 'CMON', d+2*r, c_min + d + r, c_max + d + r, c_min + c_max + d
 
 	x1 = d+2*r if d+2*r > 0 else 0
 	x1 = d+2*r if d+2*r < cutoff_approx_level else cutoff_approx_level
@@ -148,67 +147,56 @@ def calc_gd(d, mu, sigma, c_min, c_max, r, cutoff_approx_level):
 
 	x6 = c_min + c_max + d if c_min + c_max + d < cutoff_approx_level else cutoff_approx_level
 
-	print 'JAO', x1, x2 
-	print 'x1:', -1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf(((mu-math_log_if_pos(x1))+sigma**2)/(math.sqrt(2.0)*sigma)) - (d+2*r-1)*normal.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma)))
-	print 'x2:', -1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf(((mu-math_log_if_pos(x2))+sigma**2)/(math.sqrt(2.0)*sigma)) - (d+2*r-1)*normal.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma)))
-	print 'x1 first:',x1,(  normal.erf(((mu-math_log_if_pos(x1))+sigma**2)/(math.sqrt(2.0)*sigma)))
-	print 'x2 first:',x2, (  normal.erf(((mu-math_log_if_pos(x2))+sigma**2)/(math.sqrt(2.0)*sigma)))
-	print 'x1 second:',x1, -1/2.0 * ( (d+2*r-1)*normal.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma))), math_log_if_pos(x1), (mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma), math.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma))
-	print 'x2 second:',x2, -1/2.0 * ( (d+2*r-1)*normal.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma))), math_log_if_pos(x2), (mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma), math.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma))
+	# print 'JAO', x1, x2 
+	# print 'x1:', -1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf(((mu-math_log_if_pos(x1))+sigma**2)/(math.sqrt(2.0)*sigma)) - (d+2*r-1)*normal.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma)))
+	# print 'x2:', -1/2.0 * (math.exp(sigma**2/2.0 + mu) * normal.erf(((mu-math_log_if_pos(x2))+sigma**2)/(math.sqrt(2.0)*sigma)) - (d+2*r-1)*normal.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma)))
+	# print 'x1 first:',x1,(  normal.erf(((mu-math_log_if_pos(x1))+sigma**2)/(math.sqrt(2.0)*sigma)))
+	# print 'x2 first:',x2, (  normal.erf(((mu-math_log_if_pos(x2))+sigma**2)/(math.sqrt(2.0)*sigma)))
+	# print 'x1 second:',x1, -1/2.0 * ( (d+2*r-1)*normal.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma))), math_log_if_pos(x1), (mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma), math.erf((mu-math_log_if_pos(x1)) / (math.sqrt(2.0)*sigma))
+	# print 'x2 second:',x2, -1/2.0 * ( (d+2*r-1)*normal.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma))), math_log_if_pos(x2), (mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma), math.erf((mu-math_log_if_pos(x2)) / (math.sqrt(2.0)*sigma))
 	#part_1 =  from x = d+2*r to x = c_min + d + r
 	part_1 = gd(x2, d, mu, sigma, c_min, c_max, r) - gd(x1, d, mu, sigma, c_min, c_max, r)
-	print gd(c_min + d + r - 1, d, mu, sigma, c_min, c_max, r), gd(d + 2*r, d, mu, sigma, c_min, c_max, r)
-	print 'LOL', gd(x2, d, mu, sigma, c_min, c_max, r), gd(x1, d, mu, sigma, c_min, c_max, r)
+	# print gd(c_min + d + r - 1, d, mu, sigma, c_min, c_max, r), gd(d + 2*r, d, mu, sigma, c_min, c_max, r)
+	# print 'LOL', gd(x2, d, mu, sigma, c_min, c_max, r), gd(x1, d, mu, sigma, c_min, c_max, r)
 
 	#part_2 = from x = c_min + d + r to x = c_max + d + r
 	part_2 =  gd(x4, d, mu, sigma, c_min, c_max, r) - gd(x3, d, mu, sigma, c_min, c_max, r)
 
-	print gd(c_max + d + r, d, mu, sigma, c_min, c_max, r), gd(c_min + d + r, d, mu, sigma, c_min, c_max, r)
+	# print gd(c_max + d + r, d, mu, sigma, c_min, c_max, r), gd(c_min + d + r, d, mu, sigma, c_min, c_max, r)
 
 	#part_3 = from x = c_max + d + r + 1 to c_min + c_max + d 
 	part_3 = gd(x6, d, mu, sigma, c_min, c_max, r) - gd(x5, d, mu, sigma, c_min, c_max, r)
 
-	print gd(c_min + c_max + d, d, mu, sigma, c_min, c_max, r), gd(c_max + d + r + 1, d, mu, sigma, c_min, c_max, r)
-	print 'from to', c_min + c_max + d, c_max + d + r + 1
-	print part_1
-	print part_2
-	print part_3
+	# print gd(c_min + c_max + d, d, mu, sigma, c_min, c_max, r), gd(c_max + d + r + 1, d, mu, sigma, c_min, c_max, r)
+	# print 'from to', c_min + c_max + d, c_max + d + r + 1
+	# print part_1
+	# print part_2
+	# print part_3
 	return part_1 + part_2 + part_3
 
-def GapEstimator(mean, sigma, read_length, observations, c1_len, c2_len=None, emperical_max=None):
+def GapEstimator(mu, sigma, r, observations, c1_len, c2_len=None, method="NR", stepsize=None):
     '''
     Calculates the lower bound (given in http://www.ncbi.nlm.nih.gov/pubmed/22923455). The upper bound is then
     uniquely determined by the lower bound.
+    r 		- read length
+    Method  - Either "NR" for Newton-Rhapson or linear for going through every gap size (only for testing purposes)
     '''
 
     if c2_len is None:
         c2_len = c1_len
-    if emperical_max is None:
-        emperical_max = 3*math.exp(mean)
 
-    d_ML = CalcMLvaluesOfdGeneral(mean, sigma, read_length, c1_len, observations, c2_len, emperical_max)
+    if not stepsize:
+    	stepsize = math.sqrt( (math.exp(sigma**2) - 1) * math.exp(2*mu + sigma**2))/float(10)
 
-    return d_ML
-
-
-def CalcMLvaluesOfdGeneral(mu, sigma, r, c1Len, observations, c2Len, emperical_max):
-	"""
-	    returns the ML-gap as float. The ML gap is searched
-	    for with an accuracy of 0.1bp. This is NOT the accuracy of the ML-estimate
-	    however since it depends on the number of samples etc.
-	"""
-	mu, sigma, r, c1Len, c2Len = float(mu), float(sigma), float(r), float(c1Len), float(c2Len)
+	mu, sigma, r, c1_len, c2_len = float(mu), float(sigma), float(r), float(c1_len), float(c2_len)
 	# Some pruning of extreme observations would be good to have here
 	min_obs = min(observations)
 	max_obs = max(observations)
-
-	n = len(observations)
-	d_ML = 'lol'
-	c_min = min(c1Len, c2Len)
-	c_max = max(c1Len, c2Len)
+	c_min = min(c1_len, c2_len)
+	c_max = max(c1_len, c2_len)
 	min_gap = -int(min_obs) + int(2*r)
 
-	# set max to mean + 2*sd
+	# set max to mu + 2*sd
 	cutoff_approx_level = math.exp(mu + 0.5*sigma**2) + 5000*math.sqrt( (math.exp(sigma**2) - 1) * math.exp(2*mu + sigma**2) ) 
 	upper_quantile = math.exp(mu + 0.5*sigma**2) + 3*math.sqrt( (math.exp(sigma**2) - 1) * math.exp(2*mu + sigma**2) )
 	max_gap = int(upper_quantile) if upper_quantile + max_obs <  cutoff_approx_level else int(cutoff_approx_level - max_obs)  #emperical_max
@@ -216,71 +204,115 @@ def CalcMLvaluesOfdGeneral(mu, sigma, r, c1Len, observations, c2Len, emperical_m
 	d_upper = max_gap
 	print d_upper, cutoff_approx_level
 
-	# always choose the lowest root (0) to the function --> based on emperical observations
-	# IF MEAN HIGHER THAN CONDITIONAL MEAN AND STDDEV LOW: Search for negative gap
-	# IF MEAN HIGHER THAN CONDITIONAL MEAN AND STDDEV high (few observarions): Search for positive gap
-	# IF MEAN HIGHER THAN CONDITIONAL MEAN AND STDDEV high (few observarions): Search for positive gap
+    if method == "linear":
+    	d_ML = get_d_ML_linear_search(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, stepsize, cutoff_approx_level)
+    elif method == "NR":
+    	d_ML = get_d_ML_Newton_Raphson(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, cutoff_approx_level)
+    else:
+    	print "Wrong method specified, specify either 'NR' (preffered) or 'linear'. "
+    	return None
 
-	# initial values
-	# use newton rhapson
-	observation_term = calc_log_norm(observations, d_lower, mu, sigma)
-	g_d = calc_gd(d_lower, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	g_prime_d = calc_g_prim_d(d_lower, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	g_d_ratio = n*g_prime_d/g_d
-	print 'gap:',d_lower, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',observation_term
-	# d = d_lower
-	# while g_d_ratio < observation_term:
-	# 	print 'CURRENT GAP:', d
-	# 	d = 
-	# 	other_term = calc_log_norm(observations, d_ML, mu, sigma)
-	# 	g_d = calc_gd(d_ML, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	# 	g_prime_d = calc_g_prim_d(d_ML, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	# 	g_d_ratio = n*g_prime_d/g_d
-	# 	print 'gap:',d_ML, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
+    return d_ML
 
-	# 	if g_d_ratio > other_term:
-	# 		d_upper = d_ML
-	# 	else:
-	# 		d_lower = d_ML
+def get_d_ML_Newton_Raphson(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, cutoff_approx_level):
+	"""
+	    Returns the ML-gap as float. The ML gap is searched
+	    for with an accuracy of 0.5bp. This is NOT the accuracy of the ML-estimate
+	    however since it depends on the number of samples etc.
 
-	# d_ML = (d_upper + d_lower) / 2.0
+	    Algorithm:
 
-	# print d_ML
+	    Step 1: Calculate the derivative of the two functions (the g(d) ratio function
+	    		and the observation function. This is approximated here
+	    		by calculating the function values in two close points (1bp) and using:
+	    		f'(x) = (f(x_2) - f(x_1)) / (x_2 - x_1) where x_2 > x_1
+
+	    Step 2: The derivatives help us describe the fit of a line in point x. We have
+	    		y = kx+m, where k = f'(x) and m=f(x).
+	    		We use the two lines to find the intersection point x' between them. The intersection
+	    		point x' is used to define the new x.
+
+	    Step 3: Restart algorithm with x'. 
+	"""
+	n = len(observations)
+
+	x = d_lower
+	x_prime = d_upper
+	print 'Current gap:', x #, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',observation_term
+
+	import matplotlib.pyplot as plt
+
+	while True:
+		# Step 1
+		o_x1 = calc_log_norm(observations, x, mu, sigma)
+		o_x2 = calc_log_norm(observations, x+1, mu, sigma)
+
+		g_d_denominator_x1 = calc_gd(x, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_prime_d_nominator_x1 = calc_g_prim_d(x, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_d_ratio_x1 = n*g_prime_d_nominator_x1/g_d_denominator_x1
+
+		g_d_denominator_x2 = calc_gd(x+1, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_prime_d_nominator_x2 = calc_g_prim_d(x+1, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_d_ratio_x2 = n*g_prime_d_nominator_x2 /g_d_denominator_x2
+
+		o_prime = (o_x2 - o_x1) / (x+1 - x)
+		g_prime = (g_d_ratio_x2 - g_d_ratio_x1) / (x+1 - x)
+		
+		# Step 2
+		x_prime = ( (o_x1 +o_x2)/2.0 - (g_d_ratio_x1 + g_d_ratio_x2)/2.0 ) / (g_prime - o_prime)
+		print "x:", x, "x_prime:", x_prime
+		print 'Current gap:', x #, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',observation_term
+
+		###########
+		gap_size, f_1, f_2 = get_fcns(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, 100, cutoff_approx_level)
+		fig, ax = plt.subplots()
+		plt.plot(gap_size, f_1,'-')
+		plt.plot(gap_size, f_2,'-')
+		y_0 = o_prime*-200 + (o_x1 +o_x2)/2.0
+		y_1 = o_prime*1000 + (o_x1 +o_x2)/2.0
+		z_0 = g_prime*-200 + (g_d_ratio_x1 + g_d_ratio_x2)/2.0
+		z_1 = g_prime*1000 + (g_d_ratio_x1 + g_d_ratio_x2)/2.0
+		ax.scatter([-200, 1000], [y_0, y_1], marker='^', s=150, c='r')
+		ax.plot([-200, 1000], [y_0, y_1], c='r') 
+		ax.scatter([-200, 1000], [z_0, z_1], marker='^', s=150, c='b')
+		ax.plot([-200, 1000], [z_0, z_1], c='b') 
+		plt.show()
+		#################
+
+		# Step 3
+		print x_prime, x, x_prime - x
+		if x_prime - x < 0.5:
+			break
+		else:
+			x = x_prime
+
+
+	d_ML = x_prime
+	print d_ML
 	# a = raw_input("press enter")
+	return d_ML
 
 	##################################
 
-	# while d_upper - d_lower > 0.1:
-	# 	d_ML = (d_upper + d_lower) / 2.0
-	# 	d_ML_2 = d_ML + 5
-	# 	print 'D_MAX:', d_upper, "D_MIN:", d_lower
-	# 	print 'CURRENT GAP:', d_ML
-	# 	other_term = calc_log_norm(observations, d_ML, mu, sigma)
-	# 	g_d = calc_gd(d_ML, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	# 	g_prime_d = calc_g_prim_d(d_ML, mu, sigma, c_min, c_max, r, cutoff_approx_level)
-	# 	g_d_ratio = n*g_prime_d/g_d
-	# 	print 'gap:',d_ML, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
+def get_d_ML_linear_search(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, stepsize, cutoff_approx_level):
+	"""
+	    Returns the ML-gap as float. The ML gap is searched
+	    for with an accuracy of "stepsize" using a linear search in [d_lower, d_upper]. This is NOT the accuracy of the ML-estimate
+	    however since it depends on the number of samples etc.
+	"""
 
-	# 	if g_d_ratio > other_term:
-	# 		d_upper = d_ML
-	# 	else:
-	# 		d_lower = d_ML
-
-	# d_ML = (d_upper + d_lower) / 2.0
-
-	# print d_ML
-	# a = raw_input("press enter")
-
+	n = len(observations)
 	x = []
 	y = []
 	y_1 = []
 	y_2 = []
 	g_d_list = []
 	g_prim_d_list = []
+	# this is where the two functions cross and we have our (not nexessarily unique) ML estimate
+	# The fist cross is the true ML estimate based on emperical testing
+	fcns_has_crossed = False
 
-	for d_ in range(min_gap, max_gap, 100):
-		#for frac in range(10):
-		d = d_ # + frac/10.0
+	for d in range(int(d_lower), int(d_upper), int(stepsize)):
 		#print "GAP", d
 		g_d = calc_gd(d, mu, sigma, c_min, c_max, r, cutoff_approx_level)
 		g_d_list.append(g_d)
@@ -289,17 +321,18 @@ def CalcMLvaluesOfdGeneral(mu, sigma, r, c1Len, observations, c2Len, emperical_m
 
 		g_d_ratio = n*g_prime_d/g_d
 		other_term = calc_log_norm(observations, d, mu, sigma)
-		x.append(d_)
+		if other_term < g_d_ratio and not fcns_has_crossed:
+			# the ML estimate occurs at the first crossing
+			d_ML = (d + (d-stepsize))/2.0
+			fcns_has_crossed = True
+
+		x.append(d)
 		y.append(g_d_ratio - other_term)
 		y_1.append(g_d_ratio)
 		y_2.append(other_term)
-
-		# if 0.95 < g_d_ratio /calc_log_norm(observations, d, mu, sigma) < 1.05:
-		# 	d_ML = d
-		# 	print 'MLGAP', d
+		#print 'gap:',d, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
 
 
-		print 'gap:',d, 'g(d)*n', g_d_ratio, "g_d", g_d, "g_prime", g_prime_d, 'other:',other_term
 	import matplotlib.pyplot as plt
 	# plt.plot(x,y,'-')
 	# x1,x2,y1,y2 = plt.axis()
@@ -323,7 +356,58 @@ def CalcMLvaluesOfdGeneral(mu, sigma, r, c1Len, observations, c2Len, emperical_m
 	# plt.show()
 
 	print 'MLGAP', d_ML
-	print d_upper, cutoff_approx_level
+	return d_ML
+
+
+
+def get_fcns(mu, sigma, r, c_min, observations, c_max, d_lower, d_upper, stepsize, cutoff_approx_level):
+	"""
+	    Returns the ML-gap as float. The ML gap is searched
+	    for with an accuracy of "stepsize" using a linear search in [d_lower, d_upper]. This is NOT the accuracy of the ML-estimate
+	    however since it depends on the number of samples etc.
+	"""
+
+	n = len(observations)
+	x = []
+	y = []
+	y_1 = []
+	y_2 = []
+	g_d_list = []
+	g_prim_d_list = []
+	# this is where the two functions cross and we have our (not nexessarily unique) ML estimate
+	# The fist cross is the true ML estimate based on emperical testing
+	fcns_has_crossed = False
+
+	for d in range(int(d_lower), int(d_upper), int(stepsize)):
+		#print "GAP", d
+		g_d = calc_gd(d, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_d_list.append(g_d)
+		g_prime_d = calc_g_prim_d(d, mu, sigma, c_min, c_max, r, cutoff_approx_level)
+		g_prim_d_list.append(g_prime_d)
+
+		g_d_ratio = n*g_prime_d/g_d
+		other_term = calc_log_norm(observations, d, mu, sigma)
+		if other_term < g_d_ratio and not fcns_has_crossed:
+			# the ML estimate occurs at the first crossing
+			d_ML = (d + (d-stepsize))/2.0
+			fcns_has_crossed = True
+
+		x.append(d)
+		y.append(g_d_ratio - other_term)
+		y_1.append(g_d_ratio)
+		y_2.append(other_term)
+
+
+	return x, y_1, y_2
+
+
+
+
+
+
+
+
+
 
 
 # formula from: http://www.wolframalpha.com/input/?i=integral+of+max%280%2C+min%28x-d-2r%2B1%2Ca%2Bb%2Bd-x%2B1%2Cc-r%2B1%29%29*%281%2F%28x*sigma*sqrt%282*pi%29%29%29*+e%5E-%28%28ln%28x%29+-+mu%29%5E2%2F%282*sigma%5E2%29%29+with+respect+to+x
